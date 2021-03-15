07d1240698e192048ece8e000f4c72b54fc04fae
nagendren <nagendren.rajmohan@gmail.com>
2021-03-15 20:36:49 +0530

    Adding get-sub-foldersize script

---
+++ Get-Sub-foldersize.ps1
@@ -0,0 +1,24 @@
+﻿#get specific sub folder size
+function Get-Size{
+param([string]$path)
+"{0:n2}" -f ((gci -Path $path -Recurse | Measure-Object -Property length -Sum).sum /1Gb)
+}
+
+$folderlist = Get-ChildItem 'C:\Windows'
+
+foreach($i in $folderlist){
+    if($i.FullName -imatch 'assembly|debug'){
+    $size = Get-Size $i.FullName
+        if($size -gt 1){
+            $foldername = $i.FullName.TrimEnd()
+            Write-Output "$foldername total size is $size. Please review it"
+            }
+        }
+    if($i.FullName -imatch 'System32'){
+    $size = Get-Size $i.FullName
+        if($size -gt 1){
+            $foldername = $i.FullName.TrimEnd()
+            Write-Output "$foldername total size is $size. Please review it"
+            }
+        }
+}
